eaee7ce3ff681e3e5a1b2c764695e12589ade345
Rob <robheimerman@yahoo.com>
2023-12-01 19:50:29 -0600

    Moving aoc problems to their own year

---
+++ README.md
@@ -0,0 +1,2 @@
+Repo with my solutions to the [Advent of Code](https://adventofcode.com/) problems.
+I just recently came across these problems so there are missing year.  Not sure If I will go back and finish the.
--- util.js
+++ util.js
@@ -5,3 +5,7 @@ export const readSingleLinedFile = filePath => fs.readFileSync(filePath, 'utf8')
 export const readMultiLinedFile = filePath =>
     fs.readFileSync(filePath, 'utf8')
         .split('\n');
+
+export const appendFile = (filePath, data) => {
+    fs.appendFileSync(filePath, `${JSON.stringify(data)}\n`);
+}
