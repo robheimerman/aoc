36261e8ca2e5f11e619cbb57a686b18d9a29c29e
Rob <robheimerman@yahoo.com>
2023-12-09 10:45:36 -0600

    Too inefficient

--- aoc-2023/problem5/index.js
+++ aoc-2023/problem5/index.js
@@ -1,26 +1,45 @@
 import {readMultiLinedFile} from "../../util.js";
+import _ from 'lodash';
 
 const partOne = file => {
-
-    let almanac = readMultiLinedFile(file);
-
-    let seeds = almanac[0]
+    const almanac = readMultiLinedFile(file);
+    const seeds = almanac[0]
         .slice(7)
         .split(' ')
         .map(str => parseInt(str));
 
-    const indicies = almanac.reduce((a, c, i) => {
-        if (i === 0) {
-            return a;
-        }
-        if (c === "") {
-            a.push(i);
-        }
-        return a;
-    }, []);
+    return fileParser(almanac, seeds, getIndexes(almanac));
+}
 
-    indicies.forEach((v, i) => {
-        const ranges = getAlmanacRanges(almanac, i, indicies.length, v, indicies[i + 1]);
+const partTwo = file => {
+    const almanac = readMultiLinedFile(file);
+    const indexes = getIndexes(almanac);
+    const seedRanges = almanac[0]
+        .slice(7)
+        .split(' ');
+
+    let min = +Infinity;
+
+
+    _.chain(seedRanges)
+        .map(str => parseInt(str))
+        .chunk(2)
+        .forEach(([start, length]) => {
+            for (let i = 0; i < length; i++) {
+                const seed = i + start;
+                // console.log(`index: ${i} out of length: ${length}`);
+                min = Math.min(min, fileParser(almanac, [seed], indexes));
+            }
+        })
+        .value();
+
+    return min;
+}
+
+const fileParser = (almanac, seeds, indexes) => {
+
+    indexes.forEach((v, i) => {
+        const ranges = getAlmanacRanges(almanac, i, indexes.length, v, indexes[i + 1]);
         const newDestination = new Array(seeds.length).fill(null);
 
         ranges.forEach(range => {
@@ -40,6 +59,18 @@ const partOne = file => {
     return Math.min(...seeds);
 };
 
+const getIndexes = almanac => {
+    return almanac.reduce((a, c, i) => {
+        if (i === 0) {
+            return a;
+        }
+        if (c === "") {
+            a.push(i);
+        }
+        return a;
+    }, []);
+}
+
 const getDestination = (seed, [destinationStartRange, sourceRangeStart, length]) => {
     if (seed >= sourceRangeStart && seed < (sourceRangeStart + length)) {
         return seed - sourceRangeStart + destinationStartRange;
@@ -53,8 +84,9 @@ const getAlmanacRanges = (almanac, i, length, startSliceIndex, endSliceIndex) =>
     return almanac.slice(startSliceIndex + 2, endSliceIndex);
 }
 
-console.log(partOne('input-sample.txt'));
-console.log(partOne('input.txt'));
+// console.log(partOne('input-sample.txt'));
+// console.log(partOne('input.txt'));
 
-// console.log(partTwo('input-sample.txt'));
-// console.log(partTwo('input.txt'));
+console.log(partTwo('input-sample.txt'));
+console.log(partTwo('input.txt'));
+
