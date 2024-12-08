1abab6fc87d030612a37b2aad4e990ce6c1d6f1f
Rob <robheimerman@yahoo.com>
2024-12-08 14:32:54 -0800

    Problem 1 2024

---
+++ index.js
@@ -0,0 +1,3 @@
+import {partTwo} from "./aoc-2024/problem1/index.js";
+
+console.log(partTwo());
--- util.js
+++ util.js
@@ -1,13 +1,18 @@
 import fs from "fs";
+import {join} from 'path';
 
 export const readSingleLinedFile = filePath => fs.readFileSync(filePath, 'utf8');
 
-export const readMultiLinedFile = filePath =>
+export const readMultiLinedFile = (filePath) =>
     fs.readFileSync(filePath, 'utf8')
         .split('\n');
 
-export const readFileAndFilter = filePath =>
+export const readFileAndFilter = (filePath) =>
     readMultiLinedFile(filePath)
+        .filter(line => Boolean(line.length));
+
+export const readInputFile = (filePath) =>
+    readFileAndFilter(join(filePath, 'input'))
         .filter(line => Boolean(line.length));
 
 
